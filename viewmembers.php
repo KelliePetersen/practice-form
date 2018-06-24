1d52894acb13a49a5e620404123abb9e46a16e6c
KelliePetersen <KellieAnnPetersen@gmail.com>
2018-06-24 09:52:51 -0400

    add member info page

---
+++ viewmembers.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>List of Members</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <h1>List of Members</h1>
+  <?php
+    $file = "info.txt";
+    readfile($file);
+  ?>
+</body>
+</html>
